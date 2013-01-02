b5ab1c6b0d83f4eb8846d6d441115aeb5287b43e
Adrianna Pińska <adrianna.pinska@gmail.com>
2013-01-02 15:37:50 +0200

    code sample for testing; not necessarily for final inclusion

---
+++ samples/ourprog/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ourprog',
+    version='0.1',
+    description='Our first program',
+    url='http://example.com',
+    author='Jane Smith',
+    author_email='jane.smith@example.com',
+    license='GPL',
+    packages=['ourprog'],
+    test_suite='ourprog.test',
+    zip_safe=False)
