0d0542cc2846f03da1ca2771ae9d9118f5b29e53
Adrianna Pi?ska <adrianna.pinska@gmail.com>
2013-01-10 00:48:00 +0200

    almost done with GUI chapter

--- samples/firstgui.py
+++ samples/firstgui.py
@@ -1,11 +1,22 @@
-from tkinter import Tk, Label, Button
+from tkinter import Tk, Label, Button, StringVar
 
 class MyFirstGUI:
+    LABEL_TEXT = [
+        "This is our first GUI!",
+        "Actually, this is our second GUI.",
+        "We made it more interesting...",
+        "...by making this label interactive.",
+        "Go on, click on it again.",
+    ]
     def __init__(self, master):
         self.master = master
         master.title("A simple GUI")
 
-        self.label = Label(master, text="This is our first GUI!")
+        self.label_index = 0
+        self.label_text = StringVar()
+        self.label_text.set(self.LABEL_TEXT[self.label_index])
+        self.label = Label(master, textvariable=self.label_text)
+        self.label.bind("<Button-1>", self.cycle_label_text)
         self.label.pack()
 
         self.greet_button = Button(master, text="Greet", command=self.greet)
@@ -16,6 +27,11 @@ class MyFirstGUI:
 
     def greet(self):
         print("Greetings!")
+
+    def cycle_label_text(self, event):
+        self.label_index += 1
+        self.label_index %= len(self.LABEL_TEXT) # wrap around
+        self.label_text.set(self.LABEL_TEXT[self.label_index])
 
 root = Tk()
 my_gui = MyFirstGUI(root)
