911dddf1e90b5b7f9d4f7278e5ac4471d4949c61
Adrianna Pi?ska <adrianna.pinska@gmail.com>
2013-02-13 12:17:05 +0200

    updated todo

--- python_notes/conf.py
+++ python_notes/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Object-Oriented Programming in Python'
-copyright = u'2012, Adrianna Pińska'
+copyright = u'2013, University of Cape Town and individual contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -184,7 +184,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'Object-OrientedProgramminginPython.tex', u'Object-Oriented Programming in Python Documentation',
-   u'Adrianna Pińska', 'manual'),
+   u'University of Cape Town and individual contributors', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -214,7 +214,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'object-orientedprogramminginpython', u'Object-Oriented Programming in Python Documentation',
-     [u'Adrianna Pińska'], 1)
+     [u'University of Cape Town and individual contributors'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -228,7 +228,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'Object-OrientedProgramminginPython', u'Object-Oriented Programming in Python Documentation',
-   u'Adrianna Pińska', 'Object-OrientedProgramminginPython', 'One line description of project.',
+   u'University of Cape Town and individual contributors', 'Object-OrientedProgramminginPython', 'One line description of project.',
    'Miscellaneous'),
 ]
 
@@ -246,9 +246,9 @@ texinfo_documents = [
 
 # Bibliographic Dublin Core info.
 epub_title = u'Object-Oriented Programming in Python'
-epub_author = u'Adrianna Pińska'
-epub_publisher = u'Adrianna Pińska'
-epub_copyright = u'2012, Adrianna Pińska'
+epub_author = u'University of Cape Town and individual contributors'
+epub_publisher = u'University of Cape Town and individual contributors'
+epub_copyright = u'2013, University of Cape Town and individual contributors'
 
 # The language of the text. It defaults to the language option
 # or en if the language is not set.
